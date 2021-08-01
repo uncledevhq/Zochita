4be05ba6829162eb0eae2dcc7fca4eb98d09da53
Chomba <dev.chomba@gmail.com>
2021-08-02 01:59:15 +0200

    refactored

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,52 +1,59 @@
-import React from 'react';
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-function Todo({todo,setTodos, todos}) {
-    const handleCompleteChange = (e) => {
-        const value = parseFloat(e.target.value);
-        setTodos(todos.map((item) => {
-            if(item.id === value) {
-                return {
-                    ...item, completed: !item.completed
-                }
-            }
-            return item;
-        }))
+import React from "react";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+function Todo({ todo, setTodos, todos }) {
+  const handleCompleteChange = (e) => {
+    const value = parseFloat(e.target.value);
+    setTodos(
+      todos.map((item) => {
+        if (item.id === value) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
+  const handleDeleteTodo = (id) => {
+    setTimeout(() => {
+      const value = parseFloat(id);
+      // console.log(e.target.value, "changed")
+      const newTodo = todos.filter((item) => item.id !== value);
+      setTodos(newTodo);
+    }, 250);
+    // console.log(todos.length);
+  };
 
-    }
-    const handleDeleteTodo = ( id) => {        
+  return (
+    <li className={`todo-item bounceIn `}>
+      <div className="">
+        <input
+          onChange={handleCompleteChange}
+          type="checkbox"
+          name="completed"
+          id={`completed${todo.id}`}
+          value={todo.id}
+        />
+      </div>
+      <div className="flex-wider todo-title">
+        <label
+          className={`capture-wide-parent ${
+            todo.completed && "completed-todo"
+          }`}
+          htmlFor={`completed${todo.id}`}
+        >
+          {todo.title}
+        </label>
+      </div>
 
-        setTimeout(() => {
-            const value = parseFloat(id);
-            // console.log(e.target.value, "changed")
-            const newTodo = todos.filter((item) => item.id !== value)
-            setTodos(newTodo);
-        }, 250);
-        // console.log(todos.length);
-    }
-
-    return (
-        
-        <li className={`todo-item bounceIn ${todo.completed && "completed-todo"}`}>
-            <div className="">
-                <input  onChange={handleCompleteChange} type="checkbox" name="completed" id={`completed${todo.id}`} value={todo.id} />
-            </div>
-            <div className="flex-wider todo-title">
-                <label className="capture-wide-parent" htmlFor={`completed${todo.id}`}>
-                    {/* <input className={`todo-item ${todo.completed && "completed-todo"}`} value= type="text" readOnly /> */}
-                    {todo.title}
-                </label>
-            </div>
-
-            <div className="edit-btn-container">
-                <button onClick={() => handleDeleteTodo(todo.id)}>
-                    <DeleteForeverIcon style={{ color: "#18171d" }} />
-                </button>
-            </div>
-           
-        </li>
-    )
+      <div className="edit-btn-container">
+        <button onClick={() => handleDeleteTodo(todo.id)}>
+          <DeleteForeverIcon style={{ color: "#18171d" }} />
+        </button>
+      </div>
+    </li>
+  );
 }
-
-
 
 export default Todo;
