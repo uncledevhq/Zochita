3180c3b5b27b9f697cfa6c3fb271ba691579b3cb
Chomba <dev.chomba@gmail.com>
2021-09-12 08:54:12 +0200

    remove nav ribbon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import Todos from "./components/Todos";
 
 import "./App.css";
-// import ChombaRibbon from './components/ChombaRibbon';
-
 function App() {
   return (
-    <main>
-      {/* <ChombaRibbon projectName={"Zochita"} />  */}
-      <div className="body-ya-app">
-        <Todos />
-      </div>
-    </main>
+    <div className="body-ya-app">
+      <Todos />
+    </div>
   );
 }
 
